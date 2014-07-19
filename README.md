818f261229fc0be75ceb1eb7f005efd7b689798b
Emerald Icemoon <emerald.icemoon@gmail.com>
2014-07-19 11:18:33 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,68 @@
+# Iceloader-Ext
+
+A JME3 plugin library that further extensions Iceload with some more locators. These
+ones require additional 3rd party libraries, and or not required for core features,
+so are separated into a 2nd plugin.
+
+Features include :-
+
+* Local resources can be indexed using "reflections" library. 
+* Special single-file cache that uses a Java implementation of FAT32.
+
+## Dependencies
+
+This plugin uses a few external libraries. The actual number needed at runtime will
+depend on the usage.
+
+* Iceloader core plugin.
+* Reflections.  http://code.google.com/p/reflections/. Used for indexing classpath
+  resources.  "Other" open source license. Can't find actually what though.
+* Fat32Lib. https://github.com/waldheinz/fat32-lib. Used for a single-file local cache.
+  LGPL.
+
+## Usage
+
+After installation of the plugin, a new *Library* named **Iceloader** 
+will be available to add to your project. 
+
+1. Right click your project in the Projects view, select *Properties*.
+1. Click on *Libraries* in the Categories list.
+1. Select the *Compile* tab if it is not already selected.
+1. Click *Add Library*.
+1. Locate **Iceloader-Ext**, select it, then click *Add Library*.
+1. Clikc *Ok* to save and dismiss the project properties window.
+
+That's it, your project now includes the Iceloader-Ext library.
+
+### Single File Local Cache
+
+A single dynamically growing local file may be used as a local asset cache. This is yet
+another barrier to easy access to assets, although not a very good one, as it is actually
+a virtual FAT32 file system. So given the right tools a user could access the files 
+inside. However, the files inside are encrypted, and I have a use for this so it is 
+included :)
+
+To activate this, set the system property _iceloader.assetCache_ to fat32:///path/to/some/file.
+
+### The Locators
+
+Many of the locators can (and sometimes should) be configured. This is currently done
+using some system properties. I may look for a better way at some point.
+
+Depending on the locators you use, check the following. The defaults are unlikely to be
+fine for your case.
+
+#### icemoon.iceloaderext.locators.ReflectionsClasspathLocator 
+
+Much the same as the standard classpath locator, but with indexing support (provided by
+"reflections" library).
+
+#### icemoon.iceloaderext.locators.ReflectionsClasspathCachingLocator 
+
+Much the same as the ClasspathLocator, but will also allow later remote asset locators
+check for the asset too. If there is one on a server, it would be used in preference.
+
+#### icemoon.iceloaderext.locators.ReflectionsEncryptedClasspathLocator 
+
+Much the same as the ClasspathLocator, but will assume the classpath assets are encrypted,
+and decrypt them as they are returned to JME.
---
+++ src/icemoon/iceloaderext/locators/ReflectionsClasspathLocator.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2013-2014 Emeral Icemoon All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  *
+ * * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package icemoon.iceloaderext.locators;
+
+import icemoon.iceloader.IndexedAssetLocator;
+import com.jme3.asset.AssetManager;
+import java.util.logging.Logger;
+import icemoon.iceloader.AssetIndex;
+import icemoon.iceloader.IndexItem;
+import java.net.URL;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.regex.Pattern;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+
+/**
+ * Locator that locates assets on the classpath, much like {@link ReflectionsClasspathLocator}, but
+ * also provides an index of all classpath resources too.
+ */
+public class ReflectionsClasspathLocator extends com.jme3.asset.plugins.ClasspathLocator implements IndexedAssetLocator {
+
+    private boolean loadedAssetIndex;
+    private static final Logger LOG = Logger.getLogger(ReflectionsClasspathLocator.class.getName());
+    private AssetIndex assetIndex;
+
+    public ReflectionsClasspathLocator() {
+    }
+
+    @Override
+    public AssetIndex getIndex(AssetManager assetManager) {
+        if (!loadedAssetIndex) {
+            assetIndex = new AssetIndex(assetManager);
+            final Set<URL> jars = ClasspathHelper.forClassLoader(getClass().getClassLoader());
+            if (LOG.isLoggable(Level.FINE)) {
+                LOG.fine(String.format("Indexing from jars %s", jars));
+            }
+            Reflections reflections = new Reflections(new ConfigurationBuilder()
+                    .addUrls(jars)
+                    .setScanners(new ResourcesScanner()));
+            for (String s : reflections.getResources(Pattern.compile(".*"))) {
+                if (LOG.isLoggable(Level.FINE)) {
+                    LOG.fine(String.format("    %s", s));
+                }
+                assetIndex.getBackingObject().add(new IndexItem(s));
+            }
+            loadedAssetIndex = true;
+        }
+        return assetIndex;
+    }
+}
